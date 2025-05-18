3832383a141016b57d0be19f66900cd4ee9fdf89
santiagoalar <79158906+santiagoalar@users.noreply.github.com>
2025-05-18 01:45:42 -0500

    Configuración de TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RestauranteModule } from './restaurante/restaurante.module';
+import { PlatoEntity } from './plato/plato.entity/plato.entity';
 import { PlatoModule } from './plato/plato.module';
+import { RestauranteEntity } from './restaurante/restaurante.entity/restaurante.entity';
+import { RestauranteModule } from './restaurante/restaurante.module';
 
 @Module({
-  imports: [RestauranteModule, PlatoModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [RestauranteEntity, PlatoEntity],
+      synchronize: true,
+      dropSchema: true,
+    }),
+    RestauranteModule,
+    PlatoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
